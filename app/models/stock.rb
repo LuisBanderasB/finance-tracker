cc56c132259e9046fe49524dba797e0708ce4ea6
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-09-06 13:39:34 -0500

    add stock model, iex api key and modified credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:p_token],
+      secret_token: Rails.application.credentials.iex_client[:s_token],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+
+    quote = client.quote(ticker_symbol)
+    quote.latest_price
+  end
 end
