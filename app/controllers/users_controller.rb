3d0f652dacb9b0f349fe9decef9c94630f597596
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-09-08 14:51:39 -0500

    follow and unfollow users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,26 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    if params[:user].present?
+      @users = User.search(params[:user])
+      @users = current_user.except_current_user(@users)
+      if @users
+        respond_to do |format|
+          format.js { render partial: 'users/search_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "We couldn't find a user with that information"
+          format.js { render partial: 'users/search_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "please enter a user to search"
+        format.js { render partial: 'users/search_result' }
+      end
+    end
+  end
 end
